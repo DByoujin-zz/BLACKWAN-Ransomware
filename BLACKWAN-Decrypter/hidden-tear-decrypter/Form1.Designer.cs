212041d4ca44e9435cdbb480998912a3f309c3c6
DByoujin <help@esoftkorea.co.kr>
2021-02-02 21:55:22 +0900

    화면 사이즈 확대

--- BLACKWAN-Decrypter/hidden-tear-decrypter/Form1.Designer.cs
+++ BLACKWAN-Decrypter/hidden-tear-decrypter/Form1.Designer.cs
@@ -33,20 +33,20 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(97, 14);
+            this.textBox1.Location = new System.Drawing.Point(88, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(177, 21);
+            this.textBox1.Size = new System.Drawing.Size(277, 21);
             this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 17);
+            this.label1.Location = new System.Drawing.Point(15, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 12);
             this.label1.TabIndex = 1;
@@ -54,9 +54,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 38);
+            this.button1.Location = new System.Drawing.Point(19, 39);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(246, 29);
+            this.button1.Size = new System.Drawing.Size(346, 29);
             this.button1.TabIndex = 2;
             this.button1.Text = "Decrypt";
             this.button1.UseVisualStyleBackColor = true;
@@ -65,31 +65,33 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(2, 103);
+            this.label2.Font = new System.Drawing.Font("굴림", 9F);
+            this.label2.Location = new System.Drawing.Point(15, 103);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(295, 12);
+            this.label2.Size = new System.Drawing.Size(350, 12);
             this.label2.TabIndex = 3;
-            this.label2.Text = "이 랜섬웨어는 Hidden-tear를 사용한 랜섬웨어입니다.";
+            this.label2.Text = "BLACKWAN 랜섬웨어는 Hidden-tear를 사용한 랜섬웨어입니다.";
             // 
-            // label3
+            // label4
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.ForeColor = System.Drawing.Color.ForestGreen;
-            this.label3.Location = new System.Drawing.Point(77, 69);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(123, 20);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Files Decrypted!";
-            this.label3.Visible = false;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label4.Location = new System.Drawing.Point(15, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(217, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "모든 파일이 복구화가 완료되었습니다!";
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(299, 144);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(432, 131);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -111,7 +113,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
